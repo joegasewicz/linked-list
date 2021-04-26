f2b401878d1d3c6691396507377f537703f7a85b
josefgasewicz <joegasewicz@gmail.com>
2021-04-26 16:15:17 +0100

    list_init

--- linked_list.c
+++ linked_list.c
@@ -1,3 +1,17 @@
+#include <stdlib.h>
+#include <string.h>
+
 #include "linked_list.h"
 
+/**
+* list_init
+*/
+void list_init(List *list, void (*destroy)(void *data))
+{
+	list->size = 0;
+	list->destroy = destroy;
+	list->head = NULL;
+	list->tail = NULL;
+	return;
+}
 
